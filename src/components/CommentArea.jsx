d74e5951a36085a9c280e7b2066a4d9fe57ec3ae
St4n1slav <stanyyaro@gmail.com>
2025-02-06 17:07:59 +0100

    2

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -3,7 +3,8 @@ import { Container, Row, Button } from "react-bootstrap";
 import SingleBook from "./SIngleBook";
 
 class CommentArea extends Component {
-  token = "";
+  token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2E0ZGU0MWNhMDcwNDAwMTU4YmY5N2EiLCJpYXQiOjE3Mzg4NTgwNDksImV4cCI6MTc0MDA2NzY0OX0.3uYSlfttzrjw-ld_EF0TTSgH92Oixou08Tponvw7Egs";
   state = {
     comments: [],
   };
